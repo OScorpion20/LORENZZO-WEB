cfb127703dfb00954d6bfd38a58393a9355f424c
Uriel <20460044@colima.tecnm.mx>
2024-12-10 13:59:46 -0600

    Cuarto commit

--- lorenzzo/src/api.ts
+++ lorenzzo/src/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NODE_ENV === "production"
-    ? "https://lorenzzo-web.onrender.com" // URL del backend en producción
-    : "http://localhost:5000", // URL del backend en desarrollo
+  baseURL: "https://lorenzzo-web.onrender.com", // URL base del backend
 });
 
 export default API;
